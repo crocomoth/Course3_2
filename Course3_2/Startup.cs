8a4509eff5df0e596126cc97aaef26c943dc2752
crocomoth <justletmeplaymf@gmail.com>
2018-05-01 16:34:36 +0300

    Add project files.

---
+++ Course3_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Course3_2.Startup))]
+namespace Course3_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
